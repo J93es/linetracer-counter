4e7c89216f8830e20e1033fae5ef0f1ed85fd6c0
tv0924@icloud.com <j93es@naver.com>
2024-07-02 10:37:53 +0900

    [Build] add config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,5 @@
-// import "module-alias/register";
-// import "tsconfig-paths/register";
+import "module-alias/register";
+import "tsconfig-paths/register";
 import logger from "morgan";
 import createError from "http-errors";
 import express, {
