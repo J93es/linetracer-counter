dafdd2b2e8ae68f2f6ac313db71d9e390c32ce6d
tv0924@icloud.com <j93es@naver.com>
2024-07-14 12:12:17 +0900

    [Refactor] update.sh

--- frontend-client-update.sh
+++ frontend-client-update.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 WORK_DIR=/srv/linetracer-counter
-TMP_DIR=${WORK_DIR}/_tmp/frontend-admin
-BUILD_DIR=${WORK_DIR}/build/frontend-admin
+TMP_DIR=${WORK_DIR}/_tmp/frontend-client
+BUILD_DIR=${WORK_DIR}/build/frontend-client
 
 cd      ${WORK_DIR}                                                         &&
 sudo    rm -rf ${WORK_DIR}/_tmp                                             &&
 sudo    git clone https://github.com/J93es/linetracer-counter.git _tmp      && 
 cd      ${TMP_DIR}                                                          &&
-sudo    cp ${WORK_DIR}/env/frontend-admin-env ${TMP_DIR}/.env               &&
+sudo    cp ${WORK_DIR}/env/frontend-client-env ${TMP_DIR}/.env               &&
 sudo    npm install                                                         && 
 sudo    npm run build                                                       && 
 sudo    rm -rf ${BUILD_DIR}/*                                               &&
