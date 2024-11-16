0c7023c8745327ce2f84a358ee098362bb8ffd64
tv0924@icloud.com <j93es@naver.com>
2024-11-16 13:36:37 +0900

    [Update]

--- frontend-admin/src/pages/admin/controller/counter_device/SerialPortController.ts
+++ frontend-admin/src/pages/admin/controller/counter_device/SerialPortController.ts
@@ -51,8 +51,8 @@ export class SerialPortController {
       });
       const { usbProductId, usbVendorId } = this.port.getInfo();
       // await this.port.open({ baudRate: 460800 });
-      // await this.port.open({ baudRate: 9600 });
-      await this.port.open({ baudRate: 115200 });
+      await this.port.open({ baudRate: 9600 });
+      // await this.port.open({ baudRate: 115200 });
       console.log(`usb product id: ${usbProductId}`);
       console.log(`usb vender id: ${usbVendorId}`);
     } catch (error: any) {
